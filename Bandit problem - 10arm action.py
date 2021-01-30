eb02675aee16c55d3fe173d694732df5f656d6ec
ljhoh1 <s1744523@ed.ac.uk>
2021-01-30 12:45:06 +0000

    duplicate max selection fixed

--- Bandit problem - 10arm action.py
+++ Bandit problem - 10arm action.py
@@ -6,6 +6,8 @@ Created on Sat Jan 30 10:49:14 2021
 """
 import numpy as np
 import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def reward(mu, sd):
     '''
@@ -22,42 +24,56 @@ def reward(mu, sd):
     
     return reward
 
-def eval():
+#def eval():
     
 
-def eval_select(narms, steps):
+def eval_select(narms, steps, means, egreed = 0):
     '''
-    Find the q function, and choose an action
+    Find the q function, and choose an action (arm to pull) sample from.
+    
+    Inputs: 
+        - narms (int) = number of arms
+        - steps (int) = the number of steps performed
+    Outputs: 
+        - arms (list) = final value estimate for each arm
+        - selection (list) = for each step, the chosen action is appended 
     '''
     rng = np.random.default_rng()
-    arms = [0] * narms
+    arms = [0] * narms  
     
-    for arm in range(len(arms)):
-        arms[arm] = reward(5, 1)
-    
-    selection = []
-    
+    selection = [0] * narms
+        
+           
     for step in range(1, steps):
-        
-        id = arms.index(max(arms))
-        old = arms[id]
-        
-        selection.append(id)
-        
-        reward =  reward(5, 1)
-        arms[id] = old + 1/step * (reward - old)
+        # generate uniform variable for egreedy            
+        u = rng.uniform(0, 1) 
+       
+        if u > egreed:  
+            maxRew = max(arms)
+            ids = [i for i, j in enumerate(arms) if j == maxRew]
+            if len(ids) > 1:
+                id = rng.choice(ids)
+            
+            else:
+                id = ids[0]               
+            
+        elif u <= egreed:               
+            arm_option = np.arange(10)
+            id = rng.choice(arm_option)
+          
+          
+        old = arms[id]            
+        selection[id] += 1         
+        rew =  reward(means[id], 1)
+        arms[id] = old + 1/selection[id] * (rew - old)
     
     return arms, selection
 
+means = np.arange(10)
+est, sel = eval_select(narms = 10, means = means, steps = 100_000, egreed = 0.1)
 
-est, sel = eval_select(narms = 10, steps = 10000)
 
-sns.histplot(sel)
+x = np.arange(len(sel))
+plt.bar(x, sel)
 
-selection = []
-selection.append(7)
-
-for arm in range(len(arms)):
-        arms[arm] = rng.normal(5, 1)
-        
-id = arms.index(max(arms))
+sns.lineplot(x, sel)
