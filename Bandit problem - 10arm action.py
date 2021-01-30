ba02db051e32e35f3962025f5ce5259feb6eecd1
ljhoh1 <s1744523@ed.ac.uk>
2021-01-30 11:32:58 +0000

    function seperation

--- Bandit problem - 10arm action.py
+++ Bandit problem - 10arm action.py
@@ -4,35 +4,60 @@ Created on Sat Jan 30 10:49:14 2021
 
 @author: ljhoh
 """
+import numpy as np
+import seaborn as sns
 
-def estimate():
+def reward(mu, sd):
+    '''
+    generate reward from a Gaussian dist.
+    
+    Inputs: 
+        - mu (float) = the mean of normal
+        - sd (float) = the sd of normal
+    Output:
+        - reward (float) = random sample from normal
+    '''
+    rng = np.random.default_rng()
+    reward = rng.normal(mu, sd)
+    
+    return reward
+
+def eval():
+    
+
+def eval_select(narms, steps):
     '''
     Find the q function, and choose an action
     '''
-    
-    
-    
     rng = np.random.default_rng()
-    ests = rng.normal(5, 1)
-    n = 0
-    old = -100
-    #values[0] = rng.normal(5, 1)
-    #ests[0] 
+    arms = [0] * narms
     
-    while np.abs(ests - old) > 0.001:
-        
-        
-        
-        n += 1
-        
-        reward =  rng.normal(5, 1)
-        old = ests
-        
-        ests = old + 1/n * (reward - old)
-        
-        
+    for arm in range(len(arms)):
+        arms[arm] = reward(5, 1)
     
-    return ests, n
+    selection = []
+    
+    for step in range(1, steps):
+        
+        id = arms.index(max(arms))
+        old = arms[id]
+        
+        selection.append(id)
+        
+        reward =  reward(5, 1)
+        arms[id] = old + 1/step * (reward - old)
+    
+    return arms, selection
 
 
-est, n = estimate()
+est, sel = eval_select(narms = 10, steps = 10000)
+
+sns.histplot(sel)
+
+selection = []
+selection.append(7)
+
+for arm in range(len(arms)):
+        arms[arm] = rng.normal(5, 1)
+        
+id = arms.index(max(arms))
